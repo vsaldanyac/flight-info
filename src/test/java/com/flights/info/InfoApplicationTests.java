fe8259ba65221853068c881c43ab70c7a625fee2
vsaldanyac <vsaldanya@gmail.com>
2020-10-31 18:39:07 +0100

    First commit

---
+++ src/test/java/com/flights/info/InfoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flights.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
