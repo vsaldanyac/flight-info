79f454d5a2884ab94628dd2a05b82acc4bf2dad7
vsaldanya <victor.saldanya@fav24.com>
2020-11-02 01:34:59 +0100

    Must have a valid token on Header to get the info

--- flight/src/main/java/com/flights/flight/controller/FlightController.java
+++ flight/src/main/java/com/flights/flight/controller/FlightController.java
@@ -4,12 +4,10 @@ import com.flights.flight.service.FlightService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controls all relative to flight information
@@ -21,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlightController {
 
   final static Logger logger = LoggerFactory.getLogger(FlightController.class);
+
+  @Value("${valid.token}")
+  private String validToken;
 
   @Autowired
   protected FlightService flightService;
@@ -35,7 +36,11 @@ public class FlightController {
   @GetMapping("/{tailNumber}/{flightNumber}")
   public @ResponseBody
   ResponseEntity<?> getFlightInformation(@PathVariable String tailNumber,
-      @PathVariable String flightNumber) {
-    return ResponseEntity.ok(flightService.getFlightInformation(tailNumber, flightNumber));
+                                         @PathVariable String flightNumber, @RequestHeader(name = "X-Auth") String token) {
+    if (!token.equals(validToken)) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    } else {
+      return ResponseEntity.ok(flightService.getFlightInformation(tailNumber, flightNumber));
+    }
   }
 }
